2ae29759224a5ebebfb62ec472d2ff21e0495889
Essam Goda <engessamgoda@gmail.com>
2016-06-28 18:48:37 +0200

    login activity

--- app/src/main/java/e_b/itqan/restaurant/MainActivity.java
+++ app/src/main/java/e_b/itqan/restaurant/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button L = (Button) findViewById(R.id.login_button);
 
     }
 
