eaa7f928ca37207ed75726ac0505154a6e00c8d2
Essam Goda <engessamgoda@gmail.com>
2016-06-28 20:22:25 +0200

    Pref

--- app/src/main/java/e_b/itqan/restaurant/MainActivity.java
+++ app/src/main/java/e_b/itqan/restaurant/MainActivity.java
@@ -1,13 +1,12 @@
 package e_b.itqan.restaurant;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     sharedpreferences Pref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
