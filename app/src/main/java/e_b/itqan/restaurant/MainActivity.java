80ac77df6073a7020e89492a3fd4c3745773b0c2
Essam Goda <engessamgoda@gmail.com>
2016-06-28 18:31:11 +0200

    login activity

--- app/src/main/java/e_b/itqan/restaurant/MainActivity.java
+++ app/src/main/java/e_b/itqan/restaurant/MainActivity.java
@@ -1,7 +1,10 @@
 package e_b.itqan.restaurant;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button L = (Button) findViewById(R.id.login_button);
+        L.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent l = new Intent(MainActivity.this, Login_activity.class);
+                startActivity(l);
+            }
+        });
+
     }
+
 }
